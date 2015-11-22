52f47be8b905aa0cb4da35ee289ed819bece88ce
JungChanHyeong <chanh327@gmail.com>
2015-11-22 20:58:49 +0900

    Initial commit

---
+++ app/src/main/java/chanh/androidlab1/MainActivity.java
@@ -0,0 +1,29 @@
+package chanh.androidlab1;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button bt;
+    Context mContext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mContext = this.getApplication().getApplicationContext();
+        bt = (Button) this.findViewById(R.id.button);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+
+            public void onClick(View v) {
+                Toast.makeText(mContext, "Click Button", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
